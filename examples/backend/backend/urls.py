1148206fecbb1fbc57cc20c8d923a14f30cc2d6e
Luis Fagundes <lhfagundes@gmail.com>
2026-05-12 12:06:58 -0300

    simple_model backend app

--- examples/backend/backend/urls.py
+++ examples/backend/backend/urls.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from django.urls import path
 
+<<<<<<< HEAD
 from authorization import urls as authorization_urls
 from authorization_meta import urls as authorization_meta_urls
 from carousel import urls as carousel_urls
 from counter import urls as counter_urls
 from memo import urls as memo_urls
+from simple_model import urls as simple_model_urls
 from testing import urls as testing_urls
 
 APP_URLS = [
@@ -15,12 +17,14 @@ APP_URLS = [
     testing_urls,
     authorization_urls,
     authorization_meta_urls,
+    simple_model_urls,
 ]
 
 urls = [
     path('admin/', admin.site.urls),
 ]
 
+<<<<<<< HEAD
 for app_urls in APP_URLS:
     urls += app_urls.urls
 
